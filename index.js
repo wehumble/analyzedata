3562b2f828d570850968a6e457ab2be8f9fe70fd
wehumble <otienostewart62@gmail.com>
2025-12-23 23:43:52 -0800

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('datejs');
 
+function combineUsers(...arrays) {
+  const users = [].concat(...arrays);
+  const merge_date = Date.today().toString("M/d/yyyy");
+  return {
+    users: users,
+    merge_date: merge_date
+  };
+}
 
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = { combineUsers };
